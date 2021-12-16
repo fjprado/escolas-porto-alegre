52858105aac0de01ee32c98ad4275065665841f9
Fernando J Prado <f.j.prado@hotmail.com>
2021-12-16 20:45:12 -0300

    Adicionar arquivos de projeto.

---
+++ codehb-escolas-porto-alegre/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace codehb_escolas_porto_alegre
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
